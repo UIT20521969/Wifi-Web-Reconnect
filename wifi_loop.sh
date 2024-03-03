7970d1e7c1c184fe9b1b71305e23ac6aba6f1074
Thinh-UIT <116428235+UIT20521969@users.noreply.github.com>
2024-03-04 00:46:56 +0700

    Wifi loop
    Update

--- wifi_loop.sh
+++ wifi_loop.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
-
-# Thiết lập biến cờ để kiểm soát vòng lặp
-running=true
-
-# Hàm để bắt sự kiện tắt vòng lặp
-function stop_loop() {
-    running=false
-}
-
-# Gắn sự kiện tắt vòng lặp vào tín hiệu SIGINT (Ctrl+C)
-trap stop_loop SIGINT
-
-# Vòng lặp vô hạn
-while $running; do
-    # Thực hiện lệnh của bạn ở đây
-    /d/me/ce222/wifi.sh
-    
-    # Chờ 5 phút
+while true ; do
+    curl -X POST -d "username=awing15-15" -d "password=Awing15-15@2023" http://25.25.0.1/login
     sleep 2  # 300 giây = 5 phút
 done
+clear
